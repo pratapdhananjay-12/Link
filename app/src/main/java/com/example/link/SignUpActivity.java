27c2af060fabf52ef7fa78cb7fbf0d2548eb3d1d
pratapdhananjay-12 <pratap.dhananjay12@gmail.com>
2021-07-16 15:15:50 +0530

    Sign Up Activity

---
+++ app/src/main/java/com/example/link/SignUpActivity.java
@@ -0,0 +1,66 @@
+package com.example.link;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.link.databinding.ActivitySignUpBinding;
+import com.example.link.models.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("Your account is being created");
+
+        binding.signUpBtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog.show();
+                auth.createUserWithEmailAndPassword(binding.signUpEtEmail.getText().toString(),binding.signUpEtPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            Users user = new Users(binding.signUpEtUserName.getText().toString(),
+                                    binding.signUpEtEmail.getText().toString(),binding.signUpEtPassword.getText().toString());
+                            String id = task.getResult().getUser().getUid();
+                            database.getReference().child("Users").child(id).setValue(user);
+
+                            Toast.makeText(SignUpActivity.this, "User Created Successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+}
